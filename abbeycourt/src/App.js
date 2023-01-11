5dee493e2229f17b5d634490dbb6ac6b0afba5cb
Doug <testecliente207@gmail.com>
2023-01-11 19:43:21 +0000

    test parallax

--- abbeycourt/src/App.js
+++ abbeycourt/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import {Home} from "./Pages/Home/Home"
+import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
+import layer1 from "./Assets/layer1.jpg"
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+      <div className=" w-full h-full  bg-orange-600">
+                <Parallax>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.5}
+                        factor={2}
+                        style={{
+                            backgroudImage: `url(${layer1})`,
+                            backgoudSize: 'cover',
+                            backgroudPosition: 'center'
+                        }}>
+                    </ParallaxLayer>
+                </Parallax>
+      </div>
   );
 }
 
--- abbeycourt/src/Components/Header/Header.jsx
+++ abbeycourt/src/Components/Header/Header.jsx
@@ -1,10 +1,50 @@
+import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useState } from "react";
+//import Parallax, {Layer} from "react-parallar-scroll";
+import layer1 from "../../Assets/layer1.jpg";
 
 export function Header(){
+    //let element: IParallax | null
+
     return(
-            <div className=" w-80 h-24  bg-red-500 ">
-                <div className=" text-lg ">
-                    Headersssss
-                </div>
+            <div className=" w-full h-full ">
+                <Parallax>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.5}
+                        factor={2}
+                        style={{
+                            backgroudImage: `url(${layer1})`,
+                            backgoudSize: 'cover',
+                            backgroudPosition: 'center'
+                        }}>
+                    </ParallaxLayer>
+                </Parallax>
             </div>
     );
 }
+
+{/* <Parallax pages={1.45} ref={ref => element = ref}>
+                    
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.5}
+                        factor={2}
+                        style={{
+                            backgroudImage: `url(${layer7})`,
+                            backgoudSize: 'cover',
+                            backgroudPosition: 'center'
+                        }}>
+                    </ParallaxLayer>
+                    
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.8}
+                        factor={2}
+                        style={{
+                            backgroudImage: `url(${layer6})`,
+                            backgoudSize: 'cover',
+                            backgroudPosition: 'center'
+                        }}>
+                    </ParallaxLayer>
+                </Parallax> */}
