a38fe7dd6618a17e3ee8f6276f066f0be8c792ea
Douglas Araujo <testecliente207@gmail.com>
2023-03-13 04:43:53 +0000

    margin footer

--- abbeycourt/src/Components/Footer/Footer.js
+++ abbeycourt/src/Components/Footer/Footer.js
@@ -2,8 +2,8 @@
 export function Footer(){
     return(
         <div className=" bg-footer h-48 sticky">
-            <div className=" fixed ">
-                <div className=" gap-8 text-white text-sm ">
+            <div className="">
+                <div className=" gap-8 text-white text-sm pl-10 pt-20 ">
                     <div>Find Us</div>
                     <div>About</div>
                     <div>Lost & Found</div>
