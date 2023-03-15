f951c0ebfe1bf5482e39b3ae6b8629652ce8da07
Douglas Araujo <testecliente207@gmail.com>
2023-03-15 00:32:23 +0000

    details of router and pages

--- abbeycourt/src/App.js
+++ abbeycourt/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { RouteList } from "./Routes/index";
-import { Home } from "./Pages/Home/Home";
 
 
 function App() {
   return (
       <div className=" w-full h-full ">
-          {/* <Home /> */}
           <RouteList />
       </div>
   );
--- abbeycourt/src/Components/Header/Header.js
+++ abbeycourt/src/Components/Header/Header.js
@@ -1,5 +1,4 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -9,13 +8,11 @@ export function Header(){
             <div className=" w-full h-full ">
                 <div className=" w-full mr-5 ">
                     <div className=" fixed z-10 ">
-                        <div className="flex gap-8 text-white text-lg justify-end ">
-                            <div>
-                                <a  as={Link} to={"/findUs"}>Find Us</a>
-                            </div>
-                            <div as={Link} to={"/aboutUs"}>About</div>
-                            <div>Book Now</div>
-                            <div>Lost & Found</div>
+                        <div className="flex gap-8 text-white text-lg ml-5 ">
+                            <button as={Link} to={"/findUs"}>Find Us</button>
+                            <button as={Link} to={"/aboutUs"}>About</button>
+                            <button as={Link} to={"/"}>Book Now</button>
+                            <button as={Link} to={"/"}>Lost & Found</button>
                         </div>
                     </div>
                     <div className=" bg-g-header z-0 top-10 h-64 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat " />
