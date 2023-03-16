2da86276598c4e116540094c62c034fc936396af
Douglas Araujo <testecliente207@gmail.com>
2023-03-16 13:00:25 +0000

    resolve comflicts in routes

--- abbeycourt/src/Routes/index.js
+++ abbeycourt/src/Routes/index.js
@@ -3,16 +3,16 @@ import { Home } from "../Pages/Home/Home";
 import { About } from "../Pages/About/About";
 import { Findus } from "../Pages/Findus/Findus";
 
+
 export function RouteList() {
     return(
         <>
             <BrowserRouter>
-                < Home />
-                    <Routes>
-                        <Route path="/" element={< Home />}/>
-                        <Route path="/aboutUs" element={< About />}/>
-                        <Route path="/findUs" element={< Findus />}/>
-                    </Routes>
+                <Routes>
+                    <Route path="/" element={< Home />}/>
+                    <Route path="/aboutUs" element={< About />}/>
+                    <Route path="/findUs" element={< Findus />}/>
+                </Routes>
             </BrowserRouter>
         </>
     )
