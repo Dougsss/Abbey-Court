bf5ea6a5f2d48e66703a26778cd9ab6430c6ab24
Douglas Araujo <testecliente207@gmail.com>
2023-03-19 18:04:03 +0000

    settings in header about identation

--- abbeycourt/src/Components/Header/Header.js
+++ abbeycourt/src/Components/Header/Header.js
@@ -7,13 +7,14 @@ export function Header(){
     return(
             <div className=" w-full h-full ">
                 <div className=" w-full mr-5 ">
-                    <div className=" fixed ">
-                        <div className="flex gap-8 text-white text-lg mx-5 my-2 ">
+                    <div className=" fixed w-full">
+                        <div className=" flex flex-row gap-5 text-white text-lg mx-4 mb-2 justify-items-center items-center">
                             <Link to={"/"}>
                                 <img src="https://images.squarespace-cdn.com/content/v1/62b475e8a94b0b63f32309f5/c7dd5b49-680c-442b-9dc0-04854d583372/logo_whit3.png?format=1500w" alt="logo" 
                                     className=" h-16 w-20"
                                 />
                             </Link>
+                            <div className=" flex-grow "/>
                             <Link to={"/findUs"}>Find Us</Link>
                             <Link to={"/aboutUs"}>About</Link>
                             <Link to={"/booking"}>Book Now</Link>
