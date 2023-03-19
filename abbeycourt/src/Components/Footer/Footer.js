3ebfc3cf924a06a1e5f15b3de26ae5c955540531
Douglas Araujo <testecliente207@gmail.com>
2023-03-19 18:57:06 +0000

    backgroud

--- abbeycourt/src/Components/Footer/Footer.js
+++ abbeycourt/src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 
 export function Footer(){
     return(
-        <div className=" bg-footer h-48 sticky">
+        <div className=" bg-g-header bg-fixed bg-center bg-cover bg-no-repeat h-48 sticky">
             <div className="">
                 <div className=" gap-8 text-white text-sm pl-10 pt-20 ">
                     <div>Find Us</div>
