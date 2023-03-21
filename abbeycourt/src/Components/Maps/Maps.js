ef76d3242863b6f49560364a590d4d8a0cb675e2
Douglas Araujo <testecliente207@gmail.com>
2023-03-21 01:48:50 +0000

    settings in maps page

--- abbeycourt/src/Components/Maps/Maps.js
+++ abbeycourt/src/Components/Maps/Maps.js
@@ -3,8 +3,13 @@
 export function Maps() {
     return(
         <>
-        <div className=" bg-slate-300 z-0 top-10 h-96 flex justify-center items-center ">
-            Google Maps
+        <div className="z-0 top-10 h-96 flex flex-row justify-center items-center ">
+            <div className=" p-3 h-full w-1/2 bg-g-header z-0 bg-fixed bg-center bg-cover bg-no-repeat">
+                <div className="w-full h-full opacity-30 bg-slate-100"></div>
+            </div>
+            <div className="p-3 h-full w-1/2 bg-g-header z-0 bg-fixed bg-center bg-cover bg-no-repeat">
+                <div className="w-full h-full opacity-30 bg-slate-100"></div>
+            </div>
         </div>
         </>
     );
