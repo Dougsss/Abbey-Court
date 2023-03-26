42246bfb93475f2da944af9b38c9baa3df26a457
Douglas Araujo <testecliente207@gmail.com>
2023-03-26 15:47:39 +0100

    started the forms of lost and founds page

---
+++ abbeycourt/src/Components/Forms/Forms.js
@@ -0,0 +1,16 @@
+
+export function Forms() {
+    return(
+        <>
+            <div className="flex justify-center items-center h-96 w-1/2 bg-slate-100 p-3 m-5 opacity-50">
+                <div className=" text-lg">
+                    PLEASE NOTE: WE WILL ONLY CONTACT YOU IF WE LOCATE THE ITEM. ABBY COURT DOES NOT ACCEPT RESPONSIBILITY FOR LOST/STOLEN PROPERTY.
+                    SEE <span className=" hover:underline text-orange-700">TERMS AND CONDITIONS</span> FOR FURTHER DETAILS.
+                </div>
+                <div className="flex justify-center items-center">
+                    
+                </div>
+            </div>
+        </>
+    );
+}
--- abbeycourt/src/Pages/Lostfound/Lostfound.js
+++ abbeycourt/src/Pages/Lostfound/Lostfound.js
@@ -1,7 +1,11 @@
+import { Forms } from "../../Components/Forms/Forms";
+
+
 export function Lostfound() {
     return(
         <>
-        <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
+        <div className=" bg-g-header p-3 top-10 h-96 flex flex-col justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
+            <Forms />
             <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">Lost and Found</div>
         </div>
         </>
