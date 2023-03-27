e90112747fe0d8ced2defcb407ec2c83171655e2
Douglas Araujo <testecliente207@gmail.com>
2023-03-27 18:43:25 +0100

    make to Form's component

--- abbeycourt/src/Components/Forms/Forms.js
+++ abbeycourt/src/Components/Forms/Forms.js
@@ -1,18 +1,30 @@
+import { Link } from "react-router-dom";
 
 export function Forms() {
     return(
         <>
-            <div className="flex justify-center items-center h-96 w-1/2 bg-slate-100 p-3 m-5 opacity-50 flex flex-col">
-                <div className=" text-lg text-center">
-                    PLEASE NOTE: WE WILL ONLY CONTACT YOU IF WE LOCATE THE ITEM. ABBY COURT DOES NOT ACCEPT RESPONSIBILITY FOR LOST/STOLEN PROPERTY.
-                    SEE <span className=" hover:underline text-orange-700">TERMS AND CONDITIONS</span> FOR FURTHER DETAILS.
-                </div>
-                <div className="flex justify-center items-center mt-5">
-                    <div className=" flex flex-row w-80 gap-2">
-                        <div className=" border-black border-2 bg-white w-1/2"> Name</div>
-                        <div className=" border-black border-2 bg-white w-1/2">Surname</div>
+            <div className="flex justify-center items-center w-1/2 bg-slate-100 p-3 m-5 flex flex-col">
+                <form className="justify-center itens-center">
+                    <div className="mb-6">
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
                     </div>
-                </div>
+                    <div class="mb-6">
+                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+                        <input type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                    </div>
+                    <div className="mb-6">
+                        <label for="repeat-password" className="block mb-2 text-sm font-medium text-gray-900">Repeat password</label>
+                        <input type="password" id="repeat-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                    </div>
+                    <div className="flex items-start mb-6">
+                        <div className="flex items-center h-5">
+                        <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                        </div>
+                        <label for="terms" className="ml-2 text-sm font-medium text-gray-900">I agree with the <Link to={"/"} className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</Link></label>
+                    </div>
+                    <button type="submit" className="text-white bg-orange-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                </form>
             </div>
         </>
     );
--- abbeycourt/src/Pages/Lostfound/Lostfound.js
+++ abbeycourt/src/Pages/Lostfound/Lostfound.js
@@ -1,12 +1,16 @@
 import { Forms } from "../../Components/Forms/Forms";
+import { Link } from "react-router-dom";
 
 
 export function Lostfound() {
     return(
         <>
-        <div className=" bg-g-header p-3 top-10 h-96 flex flex-col justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
+        <div className=" bg-g-header p-3 top-10 flex flex-col justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
+            <div className="w-1/2 bg-slate-100 p-3 m-5 opacity-50 text-lg text-center">
+                PLEASE NOTE: WE WILL ONLY CONTACT YOU IF WE LOCATE THE ITEM. ABBY COURT DOES NOT ACCEPT RESPONSIBILITY FOR LOST/STOLEN PROPERTY.
+                SEE <Link to={"/"} className=" hover:underline text-orange-700">TERMS AND CONDITIONS</Link> FOR FURTHER DETAILS.
+            </div>
             <Forms />
-            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">Lost and Found</div>
         </div>
         </>
     );
