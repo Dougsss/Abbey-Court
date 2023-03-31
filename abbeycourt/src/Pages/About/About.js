6d997e4cf8e4d86f8bec0389a08fdf92707a4efb
Douglas Araujo <testecliente207@gmail.com>
2023-03-31 18:58:43 +0100

    add image and starting about's page

--- abbeycourt/src/Pages/About/About.js
+++ abbeycourt/src/Pages/About/About.js
@@ -1,10 +1,14 @@
-
+import Logo1 from "../../Assets/hostel1.png";
 
 export function About() {
     return(
         <>
         <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
-            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">About Us</div>
+            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">
+                <div className="m-3">
+                    <img src={Logo1} alt="hostelLogo" />
+                </div>
+            </div>
         </div>
         </>
     );
--- abbeycourt/src/Pages/Booking/Booking.js
+++ abbeycourt/src/Pages/Booking/Booking.js
@@ -2,15 +2,14 @@ export function Booking() {
     return(
         <>
         <div className=" bg-g-header p-3 pt-10 top-10 h-96 flex-col flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
-            <div className="flex justify-center items-center text-4xl text-cyan-800 p-5 mb-8">
+            <div className="flex justify-center items-center text-4xl text-cyan-800 p-5 mb-8 ">
                 <h1 className=" text-5xl text-cyan-800"> Nome do quarto</h1>
             </div>
             <div className=" w-4/5 h-full justify-center items-center bg-slate-100 flex flex-row gap-2">
                 <div className=" bg-green-900 h-3/4 w-full p-2 m-2 justify-center items-center flex"> imaggemm carousel</div>
                 <div className=" bg-green-900 h-3/4 w-full p-2 m-2 justify-start items-center flex text-base"> Texto de descricao do produto</div>
             </div>
-            
-        </div>
+        </div> 
         </>
     );
 }
