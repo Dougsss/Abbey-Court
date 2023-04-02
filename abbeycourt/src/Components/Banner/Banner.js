aee09f6fa5aae7e19ee686f782258133862fe717
Douglas Araujo <testecliente207@gmail.com>
2023-04-02 22:00:08 +0100

    add img in banner's page

--- abbeycourt/src/Components/Banner/Banner.js
+++ abbeycourt/src/Components/Banner/Banner.js
@@ -4,7 +4,8 @@ export function Banner() {
     return(
         <>
         <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
-        <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">Banner News</div>
+            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100"></div>
+            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
         </div>
         </>
     );
