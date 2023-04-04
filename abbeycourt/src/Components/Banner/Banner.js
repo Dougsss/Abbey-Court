f95d964908f4ed2f2f543fb38c6039647e7c5e87
Douglas Araujo <testecliente207@gmail.com>
2023-04-04 23:51:08 +0100

    img add in baner's page

--- abbeycourt/src/Components/Banner/Banner.js
+++ abbeycourt/src/Components/Banner/Banner.js
@@ -3,8 +3,9 @@
 export function Banner() {
     return(
         <>
-        <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
-            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100"></div>
+        <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat gap-3 ">
+            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
+            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
             <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
         </div>
         </>
