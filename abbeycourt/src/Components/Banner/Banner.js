579797f7943230e1f276f471d8ad82cb61565d04
Douglas Araujo <testecliente207@gmail.com>
2023-04-05 19:40:53 +0100

    settings banners- hover etc

--- abbeycourt/src/Components/Banner/Banner.js
+++ abbeycourt/src/Components/Banner/Banner.js
@@ -3,10 +3,10 @@
 export function Banner() {
     return(
         <>
-        <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat gap-3 ">
-            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
-            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
-            <img src="https://www.edealo.com/wp-content/uploads/2019/09/FREE-Breakfast-Promo-Code-at-Clink-Hostels.jpg" className=" w-3/4 h-3/5 rounded-lg" />
+        <div className=" bg-g-header p-5 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat gap-5 ">
+            <img src="https://tochostels.com/wp-content/uploads/2020/03/NachoVillaFoto-TOC-Hostels-Suites-19-2000x1334.jpg" className=" w-1/3 h-3/5 rounded-lg transition ease-in-out delay-110 hover:-translate-y-1 hover:scale-95 duration-200" />
+            <img src="https://shopattheponds.com.au/wp-content/uploads/sites/4/2018/02/gallery-banner.png" className=" w-1/3 h-3/5 rounded-lg transition ease-in-out delay-110 hover:-translate-y-1 hover:scale-95 duration-200" />
+            <img src="https://hostel-casa-do-rogerio.hoteis-em-suldobrasil.com/data/Images/Big3/11029/1102999/1102999209/image-caxias-do-sul-hostel-casa-do-rogerio-3.JPEG" className=" w-1/3 h-3/5 rounded-lg transition ease-in-out delay-110 hover:-translate-y-1 hover:scale-95 duration-200" />
         </div>
         </>
     );
