04bb417b766e51a740901e14fae4586bed99d920
Douglas Araujo <testecliente207@gmail.com>
2023-04-07 20:04:02 +0100

    any settings

--- abbeycourt/src/Pages/Booking/Booking.js
+++ abbeycourt/src/Pages/Booking/Booking.js
@@ -5,9 +5,11 @@ export function Booking() {
             <div className="flex justify-center items-center text-4xl text-cyan-800 p-5 mb-8 ">
                 <h1 className=" text-5xl text-cyan-800"> Nome do quarto</h1>
             </div>
-            <div className=" w-4/5 h-full justify-center items-center bg-slate-100 flex flex-row gap-2">
-                <div className=" bg-green-900 h-3/4 w-full p-2 m-2 justify-center items-center flex"> imaggemm carousel</div>
-                <div className=" bg-green-900 h-3/4 w-full p-2 m-2 justify-start items-center flex text-base"> Texto de descricao do produto</div>
+            <div className=" w-4/5 justify-center items-center bg-slate-100 flex flex-row gap-2">
+                <div className=" h-80 justify-center items-center flex">
+                    <img src="https://h-img1.cloudbeds.com/uploads/8166/img_6575_featured~~5b4f24f40a611.jpg" className=" rounded-lg h-3/4 w-2/3 p-2 m-2" />
+                </div>
+                <div className=" bg-slate-300 h-3/4 w-full p-2 m-2 justify-start items-center flex text-base"> Texto de descricao do produto</div>
             </div>
         </div> 
         </>
