8b78b0974335d7268bcefcb97492228c7306ed25
Douglas Araujo <testecliente207@gmail.com>
2023-04-09 19:27:56 +0100

    settings contacts's page, insert to h1 for forms of booking grups

--- abbeycourt/src/Pages/Contact/Contact.js
+++ abbeycourt/src/Pages/Contact/Contact.js
@@ -3,11 +3,11 @@ import { Maps } from "../../Components/Maps/Maps";
 export function Contact() {
     return(
         <>
-        <div className=" bg-g-header p-3 top-10 h-10 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat"></div>
-        <Maps />
-        <div className=" bg-g-header p-3 top-10 h-96 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat">
-            <div className="w-full h-full opacity-30 flex justify-center items-center bg-slate-100">Contact Us</div>
-        </div>
+        <div className=" bg-g-header p-3 top-10 h-10 flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat"/>
+        	<div className=" p-5 flex items-center justify-center">
+                <h1 className=" text-5xl text-orange-500 font-bold justify-center text-center">NUMBER 1 FOR GROUPS IN DUBLIN</h1>
+            </div>
+            <Maps />
         </>
     );
 }
