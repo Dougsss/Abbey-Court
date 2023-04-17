fde928fb34108e124fdd261ee8907d91c97be6df
Douglas Araujo <testecliente207@gmail.com>
2023-04-17 23:01:00 +0100

    insert definicions of ux in cards

--- abbeycourt/src/Components/Cads/Cards.js
+++ abbeycourt/src/Components/Cads/Cards.js
@@ -21,8 +21,8 @@ import {
 export function Cards() {
     return(
         <>
-            <Card className="w-full max-w-[26rem] shadow-lg">
-                <CardHeader floated={false} color="blue-gray">
+            <Card className="w-full max-w-[26rem] shadow-lg bg-clip-border rounded-xl relative flex flex-col">
+                <CardHeader className=" bg-clip-border mt-0 mx-0 overflow-hidden" floated={false} color="blue-gray">
                     <img
                         src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                         alt="ui/ux review check"
@@ -37,7 +37,7 @@ export function Cards() {
                     <HeartIcon className="h-6 w-6" />
                     </IconButton>
                 </CardHeader>
-                <CardBody>
+                <CardBody className=" mx-4 p-6">
                     <div className="mb-3 flex items-center justify-between">
                         <Typography variant="h5" color="blue-gray" className="font-medium">
                             Wooden House, Florida
@@ -56,39 +56,39 @@ export function Cards() {
                     </Typography>
                     <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
                         <Tooltip content="$129 per night">
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            <BanknotesIcon className="h-5 w-5" />
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                <BanknotesIcon className="h-5 w-5" />
+                            </span>
                         </Tooltip>
                         <Tooltip content="Free wifi">
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            <WifiIcon className="h-5 w-5" />
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                <WifiIcon className="h-5 w-5" />
+                            </span>
                         </Tooltip>
                         <Tooltip content="2 bedrooms">
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            <HomeIcon className="h-5 w-5" />
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                <HomeIcon className="h-5 w-5" />
+                            </span>
                         </Tooltip>
                         <Tooltip content={`65" HDTV`}>
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            <TvIcon className="h-5 w-5" />
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                <TvIcon className="h-5 w-5" />
+                            </span>
                         </Tooltip>
                         <Tooltip content="Fire alert">
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            <FireIcon className="h-5 w-5" />
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                <FireIcon className="h-5 w-5" />
+                            </span>
                         </Tooltip>
                         <Tooltip content="And +20 more">
-                        <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
-                            +20
-                        </span>
+                            <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-orange-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
+                                +20
+                            </span>
                         </Tooltip>
                     </div>
                 </CardBody>
                 <CardFooter className="pt-3">
-                    <Button size="lg" fullWidth={true}>
+                    <Button className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-orange-500 text-white shadow-md shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full" size="lg" fullWidth={true}>
                         Reserve
                     </Button>
                 </CardFooter>
