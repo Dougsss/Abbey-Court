67b8205b714698193ce94675d78dd4c7c159f379
Douglas Araujo <testecliente207@gmail.com>
2023-04-17 21:44:33 +0100

    settings cads

--- abbeycourt/src/Pages/Booking/Booking.js
+++ abbeycourt/src/Pages/Booking/Booking.js
@@ -3,9 +3,12 @@ import { Cards } from "../../Components/Cads/Cards";
 export function Booking() {
     return(
         <>
-            <div className=" bg-g-header p-3 pt-10 top-10 h-96 flex-col flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat ">
-                <Cards />
-            </div>
+            {/* <div className=" bg-g-header p-3 pt-10 top-10 flex-col flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat " /> */}
+                <div className=" w-10/13 p-3 pt-8 bg-g-header bg-fixed bg-center bg-cover bg-no-repeat flex flex-row gap-5 justify-center items-center">
+                    <Cards />
+                    <Cards />
+                    <Cards />
+                </div>
         </>
     );
 }
