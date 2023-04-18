1104dc74f624b25989f439960139c7ec7d8eadd3
Douglas Araujo <testecliente207@gmail.com>
2023-04-18 23:03:33 +0100

    ok

--- abbeycourt/src/Pages/Booking/Booking.js
+++ abbeycourt/src/Pages/Booking/Booking.js
@@ -4,7 +4,7 @@ export function Booking() {
     return(
         <>
             {/* <div className=" bg-g-header p-3 pt-10 top-10 flex-col flex justify-center items-center bg-fixed bg-center bg-cover bg-no-repeat " /> */}
-                <div className=" w-10/13 p-3 pt-8 bg-g-header bg-fixed bg-center bg-cover bg-no-repeat flex flex-row gap-5 justify-center items-center">
+                <div className=" w-10/13 p-3 pt-16 bg-g-header bg-fixed bg-center bg-cover bg-no-repeat flex flex-row gap-6 justify-center items-center">
                     <Cards />
                     <Cards />
                     <Cards />
